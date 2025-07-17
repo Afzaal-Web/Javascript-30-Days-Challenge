c22fd7e906fa5fccfa61466ae378d23ea3c18b12
Afzaal Web <afzaalwebsolutions@gmail.com>
2025-07-17 20:38:15 +0500

    set anchor attribute to blank

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const grid = document.getElementById('grid');
 
 for(let i = 1; i <= 30; i++){
     const link = document.createElement('a');
+    link.href = `Day ${i}/index.html`;
+    link.setAttribute('target', '_blank');
     link.textContent = 'Day ' + i;
-    link.href = `Day ${i}/index.html`
     link.classList.add('day-link');
     grid.appendChild(link);
 }
